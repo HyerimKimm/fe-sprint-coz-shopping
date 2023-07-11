5919850387b490fd1351580072603e9c6bb3e8e1
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-07-11 17:56:37 +0900

    App.js에서 import 코드 오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { Header } from './components/header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages/MainPage';
-import ProductListPage from './pages/ProductListPage';
-import BookmarkPage from './pages/BookmarkPage';
+import { ProductListPage } from './pages/ProductListPage';
+import { BookmarkPage } from './pages/BookmarkPage';
 import { Footer } from './components/footer/Footer';
 
 function App() {
