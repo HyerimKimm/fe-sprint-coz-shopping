3dbb8ff6dd51d3499cce60418d1938ccb28f6319
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-07-14 11:11:07 +0900

    모달 창에서 북마크 추가/해제 기능 구현

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { styled } from 'styled-components';
 import { updateIsModalOpen } from '../../redux/actions/isModalOpenAction';
 import modalCloseButton from '../../assets/images/modalCloseButton.png'
+import { BookmarkImg } from '../main/Item.style';
+import bookmarkClick from '../../assets/images/BookmarkClick.svg'
+import bookmarkNonClick from '../../assets/images/bookmarkNonclick.svg'
+import { getBookmark } from '../../redux/actions/bookmarkAction';
 
 export const BackgroundContainer= styled.div`
     background-color: rgba(255,255,255,0.4);
@@ -28,7 +32,7 @@ export const ModalContainer = styled.section`
     min-height: 280px;
     left:50%;
     transform: translate(-50%,0);
-    background-image:url(${(props)=>props.imgUrl});
+    background:url(${(props)=>props.imgUrl}) no-repeat center center fixed;
     background-size: cover;
     border-radius: 20px;
 `
@@ -43,26 +47,80 @@ export const ModalCloseButton = styled.button`
     cursor: pointer;
 `
 export const ModalCloseButtonImg = styled.img`
-    width: 24px;
-    height: 24px;
+    width: 20px;
+    height: 20px;
+`
+export const ModalBookmarkButton = styled.button`
+    box-sizing: border-box;
+    margin: 15px;
+    border: 0;
+    background-color: transparent;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+export const ModalItemTitle = styled.span`
+    margin-left: 2px;
+    font-size: 24px;
+    color:#fff;
+    text-shadow: 0px 0px 4px rgba(0,0,0,0.15);
+    font-weight: 700;
 `
 export const Modal = ({item}) => {
     const dispatch = useDispatch();
+    const selector = useSelector(state=>state);
+    let bookmarkIdList = selector.bookmarkReducer.map((e)=>e.id);
 
-    const onModalBackgroundClickHandler = () => {
+    const bookmarkInsert = () => {
+        localStorage.setItem('bookmark',JSON.stringify([
+            ...selector.bookmarkReducer, item
+        ]))
+        dispatch(getBookmark(JSON.parse(localStorage.getItem('bookmark'))))
+    }
+    const bookmarkRemove = () => {
+        localStorage.setItem('bookmark',JSON.stringify(selector.bookmarkReducer.filter(
+            (e)=>e.id!==item.id
+        )))
+        dispatch(getBookmark(JSON.parse(localStorage.getItem('bookmark'))))
+
+    }
+    const modalClose = () => {
         dispatch(updateIsModalOpen(false,null));
     }
-
     const onModalClickHandler = (e) => {
         e.stopPropagation();
     }
+    const onBookmarkClickHandler = (e) => {
+        e.stopPropagation();
+        if(bookmarkIdList.includes(item.id)) {
+            bookmarkRemove();
+        }
+        else {
+            bookmarkInsert();
+        }
+    }
 
     return (
-        <BackgroundContainer onClick={onModalBackgroundClickHandler}>
+        <BackgroundContainer onClick={modalClose}>
             <ModalContainer imgUrl={item.type==="Brand"?item.brand_image_url:item.image_url} onClick={onModalClickHandler}>
-                <ModalCloseButton>
+                <ModalCloseButton onClick={modalClose}>
                     <ModalCloseButtonImg src={modalCloseButton}/>
                 </ModalCloseButton>
+                <ModalBookmarkButton onClick={onBookmarkClickHandler}>
+                        <BookmarkImg src={bookmarkIdList.includes(item.id)?bookmarkClick:bookmarkNonClick}/>
+                        <ModalItemTitle>
+                        {
+                            item.type==='Brand'?item.brand_name
+                            :item.type==='Category'?`#${item.title}`
+                            :item.title
+                        }
+                        </ModalItemTitle>
+                </ModalBookmarkButton>
             </ModalContainer>
         </BackgroundContainer>
     );
